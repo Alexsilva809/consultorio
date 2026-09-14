2570c0f97a60bd486e0a897d97eb68a01ee6eb4e
Alexsilva809 <apereiradasilva087@gmail.com>
2026-09-14 12:30:28 -0300

    app adicionada

---
+++ app/Pessoa.php
@@ -0,0 +1,10 @@
+<?php
+abstract class Pessoa{
+    public $id;
+    public $nome;
+    public $cpf;
+    public $telefone;
+    public $email;
+    public $dataNacismento;
+    public $endereco;
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ifto/consultorio',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd73e9953d1ff1c43e444f1ede127afd77e359686',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ifto/consultorio' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd73e9953d1ff1c43e444f1ede127afd77e359686',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
